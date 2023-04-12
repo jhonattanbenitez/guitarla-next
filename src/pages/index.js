2e253d3b84ff943b8ad99f478e4e11492bced6ab
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-04-11 22:32:59 -0500

    about us

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,9 @@
-import Head from 'next/head';
+import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato&family=Outfit:wght@400;700;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <h2>Hello world</h2>
+      <Link href="/about-us">About Us</Link>
     </>
   );
 }
