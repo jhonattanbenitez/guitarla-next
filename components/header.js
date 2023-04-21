068ea9c1ca5b50231b497a334615082e953bc25d
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-04-21 17:17:00 -0500

    header and footer

--- components/header.js
+++ components/header.js
@@ -1,16 +1,20 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
+import styles from '../src/styles/header.module.css'
 export default function Header() {
+    const router = useRouter()
   return (
-    <header>
-        <div className="container">
-            <Image src="/img/logo.svg" width={300} height={40} alt='Logo image'/>
-            <nav>
-                <Link href="/">Home</Link>
-                <Link href="/about-us">About Us</Link>
-                <Link href="/blog">Blog</Link>
-                <Link href="/store">Store</Link>
-                
+    <header className={styles.header}>
+        <div className={`container ${styles.nav_bar}`}>
+            <Link href="/"><Image src="/img/logo.svg" width={300} height={40} alt='Logo image'/></Link>    
+            <nav className={styles.nav}>
+                <Link href="/" className={router.pathname === '/' ? styles.active : ''}>
+                    Home
+                </Link>
+                <Link href="/about-us" className={router.pathname === '/about-us' ? styles.active : ''}>About Us</Link>
+                <Link href="/blog" className={router.pathname === '/blog' ? styles.active : ''}>Blog</Link>
+                <Link href="/store" className={router.pathname === '/store' ? styles.active : ''}>Store</Link> 
             </nav>
         </div>
     </header>
