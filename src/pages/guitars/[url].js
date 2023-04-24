25c783700ad1bf4718f6f65ffec807ced2ac87ad
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-04-24 17:57:35 -0500

    routing

---
+++ src/pages/guitars/[url].js
@@ -0,0 +1,65 @@
+import Image from 'next/image';
+import styles from '../../styles/guitar.module.css';
+import Layout from '../../../components/layout';
+
+
+
+/*export async function getServerSideProps({query: {url}}) {
+    const response = await fetch(`http://localhost:1337/api/guitars?filters[url]=${url}&populate=image`);
+    const {data: guitar} = await response.json();
+
+    return {
+        props: {
+           guitar 
+        }
+    }
+} */
+
+export async function getStaticPaths() {
+  const response = await fetch('http://localhost:1337/api/guitars');
+  const { data } = await response.json();
+
+  const paths = data.map((guitar) => ({
+    params: { url: guitar.attributes.url },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { url } }) {
+  const response = await fetch(
+    `http://localhost:1337/api/guitars?filters[url]=${url}&populate=image`
+  );
+  const { data: guitar } = await response.json();
+
+  return {
+    props: {
+      guitar,
+    },
+  };
+}
+export default function Product({ guitar }) {
+  const { name, description, price, image } = guitar[0].attributes;
+  return (
+    <Layout
+        title={`Guitar Store - ${name}`}
+        description={description}
+    >
+    <div className={styles.guitar}>
+      <Image
+        src={image.data.attributes.formats.medium.url}
+        width={400}
+        height={400}
+        alt={name}
+      />
+      <div className={styles.content}>
+        <h3>{name}</h3>
+        <p className={styles.description}>{description}</p>
+        <h3 className={styles.price}>${price}</h3>
+      </div>
+    </div>
+    </Layout>
+  );
+}
--- src/pages/store.js
+++ src/pages/store.js
@@ -1,9 +1,40 @@
+import Guitar from '../../components/guitar';
 import Layout from '../../components/layout';
-export default function Store() {
+import styles from '../styles/grid.module.css'
+
+/* export async function getStaticProps() { 
+  const res = await fetch(`http://localhost:1337/api/guitars?populate=image`);
+  const {data: guitars} = await res.json();
+  return {
+    props: {
+      guitars,
+    },
+  };
+} */
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:1337/api/guitars?populate=image`);
+  const { data: guitars } = await res.json();
+  return {
+    props: {
+      guitars,
+    },
+  };
+}
+
+export default function Store({ guitars }) {
+  console.log(guitars)
   return (
     <>
-      <Layout title='Store' description='Store, Guitar La Music Store'>
-        <h1>Store</h1>
+      <Layout title="Store" description="Store, Guitar La Music Store">
+        <main className="container">
+          <h1>Our Collection</h1>
+          <div className={styles.grid}>
+          {guitars?.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar.attributes} />
+          ))}
+          </div>
+        </main>
       </Layout>
     </>
   );
