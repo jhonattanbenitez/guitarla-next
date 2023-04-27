c4af02a6ebc59e27118b0d1a4299f1feb080da4d
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-04-27 18:24:12 -0500

    home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,52 @@
-import Link from 'next/link';
+import Guitar from '../../components/guitar';
+import Post from '../../components/post';
 import Layout from '../../components/layout';
-export default function Home() {
+import styles from '../../src/styles/grid.module.css'
+
+export async function getStaticProps() {
+  const guitarsUrl = 'http://localhost:1337/api/guitars?populate=image';
+  const postsUrl = 'http://localhost:1337/api/posts?populate=image';
+
+  const [guitarsRes, postsRes] = await Promise.all([
+    fetch(guitarsUrl),
+    fetch(postsUrl),
+  ]);
+  const [{ data: guitars }, { data: posts }] = await Promise.all([
+    guitarsRes.json(),
+    postsRes.json(),
+  ]);
+  return {
+    props: {
+      guitars,
+      posts,
+    },
+  };
+}
+
+export default function Home({ guitars, posts }) {
+  
   return (
     <>
       <Layout
         title={'Home'}
         description={'This the home for the GuitarLA Store'}
       >
-        <h2>Hello world</h2>
-        <Link href="/about-us">About Us</Link>
+        <main className='container'>
+          <h1 className='heading'>Our Collection</h1>
+          <div className={styles.grid}>
+          {guitars?.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar.attributes} />
+          ))}
+          </div>
+          <section className='container'>
+          <h2 className='heading'>Blog</h2>
+          <div className={styles.grid}>
+          {posts?.map(post => (
+            <Post key={post.id} post={post.attributes} />
+          ))}
+          </div>
+          </section>
+        </main>
       </Layout>
     </>
   );
